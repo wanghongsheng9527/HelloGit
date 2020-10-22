dd1d685bad10d5a92729bcccd726ea724d846d4e
wanghongsheng9527 <2928655954@qq.com>
2020-10-22 18:32:00 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/whs/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.whs.git;
+
+/**
+ * @author 王宏盛
+ * @create 2020-10-22 17:57
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
